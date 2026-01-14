ba03e60e3613c139d9555a64ff8dce714fe8a910
Sergey Plis <s.m.plis@gmail.com>
2026-01-14 01:18:09 -0500

    Fix: Replace large video with GitHub release link

---
+++ serve.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import http.server
+import socketserver
+
+PORT = 8000
+
+Handler = http.server.SimpleHTTPRequestHandler
+Handler.extensions_map.update({
+    ".js": "application/javascript",
+})
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Serving at http://localhost:{PORT}")
+    httpd.serve_forever()
